af9b0463372da0f673f8178fcc701ddc827f043a
Francisco Santana Cardoso <fsantanagy@gmail.com>
2023-12-20 16:31:16 -0300

    blog demo para treino

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import {React, useEffect, useState } from "react";
+import './stily.css'
 
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+
+    function loadApi() {
+      let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+    
+      fetch(url)
+        .then((returnAPI)=> returnAPI.json())
+        .then((jsonAPI)=> {
+          setPosts(jsonAPI)
+        })
+    }
+
+    loadApi();
+  }, []);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <strong id="labelTitulo">Nutri React</strong>
       </header>
+
+      <div className="container">
+        {posts.map((post)=>{
+            return(
+              <article key={post.id} className="post">
+                <h1 className="titulo">{post.titulo}</h1>
+                <img src={post.capa} alt={post.titulo} className="postImg"/>
+                <p className="subtitulo">{post.subtitulo}</p>
+                <a className="botaoAcessar">Acessar</a>
+              </article>
+            )
+          })
+        }
+      </div>
+
     </div>
   );
 }
